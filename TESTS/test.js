d5d250615371e51993983e3321242c1fa9c06466
kevin-magu <wanjaukevinmagu@gmail.com>
2023-05-12 21:21:56 +0300

    wohoo the titlt effect

---
+++ TESTS/test.js
@@ -0,0 +1,7 @@
+VanillaTilt.init(document.querySelector(".card-inner"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 0.5,
+  });
+  
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let windowsFacts = document.querySelector('.windows-facts');
 let vscodePic = document.querySelector('.vscode');
 let vscodeFacts = document.querySelector('.vscode-facts');
 let githubFacts = document.querySelector('.github-facts');
-
+let appleFacts = document.querySelector('.apple-facts');
 
 function dWindowsFacts() {
     windowsFacts.style.display = 'block';
@@ -16,12 +16,19 @@ function dVscodeFacts(){
     vscodeFacts.style.display = 'block';
     setTimeout(function(){
         vscodeFacts.style.display = 'none';
-    }, 10000)
+    }, 10000);
 };
 
-function dGitHubFacts(){
+function dGithubFacts(){
     githubFacts.style.display = 'block';
     setTimeout(() => {
         githubFacts.style.display = 'none';
-    }, 10000)
+    }, 10000);
+};
+
+function dAppleFacts(){
+    appleFacts.style.display = 'block';
+    setTimeout(()=>{
+        appleFacts.style.display = 'none';
+    }, 10000);
 };
